086088a55e97ef4b252a8cebb4f50ecb8dd5ff93
AaronXiaoCN <x250909895@outlook.com>
2025-10-04 15:29:03 +0800

    继续测试隐藏CM功能

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -297,7 +297,7 @@ void runConnectionManagerScreen() async {
   final hide = await bind.cmGetConfig(name: "hide_cm") == 'true';
   //final allowhidecm = mainGetLocalBoolOptionSync(kOptionAllowHideCm);
   //final allowhidecm = bind.mainGetLocalOption(key: 'allow-hide-cm') == 'Y';
-  gFFI.serverModel.hideCm = hide 
+  gFFI.serverModel.hideCm = hide;
   //根据hide_cm配置，决定是否隐藏主窗口 
   if (hide) {
     await hideCmWindow(isStartup: true);
